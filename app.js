84e6d3d6f12a3745728e3b909bfc2a75fcb0aa0d
RH Sadik <sadikcsbd@gmail.com>
2023-12-20 15:10:13 +0600

    complete

--- app.js
+++ app.js
@@ -19,54 +19,87 @@ function showSuccess(input){
 }
 
 // Email validation
-function isValidEmail(email){
-    return email.match(
-        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
+function checkEmail(input){
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(input.value.trim())){
+        showSuccess(input)
+    }else{
+        showError(input, 'Email is not Valid');
+    }
 }
 
 // First Solution
-form.addEventListener('submit', function(e){
-    e.preventDefault();
+// form.addEventListener('submit', function(e){
+//     e.preventDefault();
 
-    if(username.value === ''){
-        showError(username, 'Username is required');
-    }else{
-        showSuccess(username);
-    }
+//     if(username.value === ''){
+//         showError(username, 'Username is required');
+//     }else{
+//         showSuccess(username);
+//     }
 
-    if(email.value === ''){
-        showError(email, 'email is required');
-    }else if(!isValidEmail(email.value)){
-        showError(email, 'Email is not valid');
-    }else{
-        showSuccess(email);
-    }
+//     if(email.value === ''){
+//         showError(email, 'email is required');
+//     }else if(!isValidEmail(email.value)){
+//         showError(email, 'Email is not valid');
+//     }else{
+//         showSuccess(email);
+//     }
 
-    if(password.value === ''){
-        showError(password, 'password is required');
-    }else{
-        showSuccess(password);
-    }
+//     if(password.value === ''){
+//         showError(password, 'password is required');
+//     }else{
+//         showSuccess(password);
+//     }
 
-    if(password2.value === ''){
-        showError(password2, 'Confirmed password is required');
-    }else{
-        showSuccess(password2);
-    }
-})
+//     if(password2.value === ''){
+//         showError(password2, 'Confirmed password is required');
+//     }else{
+//         showSuccess(password2);
+//     }
+// });
 
 
 
 // 2nd Solution
-// function checkRequired(inputArr){
-//     inputArr.forEach(function(input){
-//         if(input.value.trim() === ''){
-//             showError(input, `${input.id} is required`)
-//         }
-//     });
-// }
-// form.addEventListener('submit', function(e){
-//     e.preventDefault();
-//     checkRequired([username, email, password, password2]);
-// })
+function checkRequired(inputArr){
+    inputArr.forEach(function(input){
+        if(input.value.trim() === ''){
+            showError(input, `${getFieldName(input)} is required`)
+        }
+    });
+}
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    checkRequired([username, email, password, password2]);
+
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordMatch(password, password2);
+});
+
+
+// Get Field Name
+function getFieldName(input){
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// Check Password Match
+function checkPasswordMatch(input1, input2){
+    if(input1.value !== input2.value){
+        showError(input2, 'Password do not match');
+    }else{
+        showSuccess(input1);
+        showSuccess(input2);
+    }
+}
+
+// Check Length
+function checkLength(input, min, max){
+    if(input.value.length < min){
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+    }else if(input.value.length > max){
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+    }
+}
